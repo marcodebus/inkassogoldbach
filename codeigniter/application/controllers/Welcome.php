2e09bb6fcf5f4265a38adc464ae91db9c99d5fa7
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-09-14 08:46:03 +0200

    Update Welcome.php

--- codeigniter/application/controllers/Welcome.php
+++ codeigniter/application/controllers/Welcome.php
@@ -14,7 +14,8 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$data['sql'] = $this->SQLQuery->Select('SELECT a.Akte, a.Nachname, Sum(IF(z.Betrag Is Null,0,z.Betrag)) AS Zinszahlungen FROM Akten AS a LEFT JOIN (SELECT a1.Akte, a1.Nachname, z1.Betrag FROM Akten AS a1 LEFT JOIN Zahlungen AS z1 ON a1.Akte = z1.Akte WHERE (((z1.Art)=2))) AS z ON a.Akte = z.Akte GROUP BY a.Akte, a.Nachname');
+		$data['sql'] = $this->SQLQuery->Select('
+SELECT a.Akte, a.Nachname, Sum(IF(z.Betrag Is Null,0,z.Betrag)) AS Zinszahlungen FROM Akten AS a LEFT JOIN (SELECT a1.Akte, a1.Nachname, z1.Betrag FROM Akten AS a1 LEFT JOIN Zahlungen AS z1 ON a1.Akte = z1.Akte WHERE (((z1.Art)=2))) AS z ON a.Akte = z.Akte GROUP BY a.Akte, a.Nachname');
     $this->load->view('welcome_message', $data);
 	}
 }
