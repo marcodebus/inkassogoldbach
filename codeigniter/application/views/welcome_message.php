995a96dd8e040ae142afac1d4a45c1c691818a3e
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-09-14 08:47:04 +0200

    Update welcome_message.php

--- codeigniter/application/views/welcome_message.php
+++ codeigniter/application/views/welcome_message.php
@@ -44,7 +44,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body">
 
 		<p>SQL Syntax:
-		  <code>SELECT Akten.Akte, Akten.Nachname, (IFNULL(SUM(Zahlungen.Betrag),0) ) AS Zinszahlungen FROM Akten LEFT JOIN Zahlungen ON Akten.Akte = Zahlungen.Akte AND Zahlungen.Art = 2 GROUP BY Akten.Akte</code>
+		  <code>
+SELECT a.Akte, a.Nachname, Sum(IF(z.Betrag Is Null,0,z.Betrag)) AS Zinszahlungen FROM Akten AS a LEFT JOIN (SELECT a1.Akte, a1.Nachname, z1.Betrag FROM Akten AS a1 LEFT JOIN Zahlungen AS z1 ON a1.Akte = z1.Akte WHERE (((z1.Art)=2))) AS z ON a.Akte = z.Akte GROUP BY a.Akte, a.Nachname</code>
 		 </p>
 
 	<?php
